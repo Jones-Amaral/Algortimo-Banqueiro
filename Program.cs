21554f94fd106bd07f66a8fb9d93f6edbb894025
João Vitor Alves Amaral <am.joao13@gmail.com>
2026-04-12 18:40:30 -0300

    Criação do exercicio

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+
+class Banqueiro
+{
+    private const int NUMBER_OF_CUSTOMERS = 5;
+    private const int NUMBER_OF_RESOURCES = 10;
+    int[] available = new int[NUMBER_OF_RESOURCES];
+    int[,] maximum = new int[NUMBER_OF_CUSTOMERS,NUMBER_OF_RESOURCES];
+    int[,] allocation = new int[NUMBER_OF_CUSTOMERS,NUMBER_OF_RESOURCES];
+    int[,] need = new int[NUMBER_OF_CUSTOMERS,NUMBER_OF_RESOURCES];
+
+}
+class Program
+{
+    static void Main()
+    {
+
+    }
+}
